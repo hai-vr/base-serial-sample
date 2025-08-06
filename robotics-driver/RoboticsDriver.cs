2b46d9192dd3af9783b5b0e0258c6bcf50501dc7
Haï~ <hai-vr@users.noreply.github.com>
2025-08-06 09:43:59 +0200

    Implement polar safety mode to clamp the Y and Z axis: - Implement a safety mode that clamps the Y and Z axis to be within a disc. - The radius of the disc is different at topmost and at bottommost. - This mode is not exposed in the UI in this commit. - Also, change virtual scale to be the inverse, so that it represents the scale in the virtual space for the same movement to happen in physical space.

--- robotics-driver/RoboticsDriver.cs
+++ robotics-driver/RoboticsDriver.cs
@@ -1,11 +1,15 @@
 ﻿
+using System.Numerics;
 using Hai.PositionSystemToExternalProgram.Core;
 
 namespace Hai.PositionSystemToExternalProgram.Robotics;
 
 public class RoboticsDriver
 {
-    private float _virtualScaleChange = 1f; // = 1.5f;
+    private float _configPolarModeBottommostRadius = 0.4f;
+    private float _configPolarModeUppermostRadius = 1f;
+    private float _configVirtualScaleChange = 1f; // = 1.5f;
+    private bool _configSafetyUsePolarMode = false;
     
     private float _unsafeJoystickTargetL0;
     private float _unsafeJoystickTargetL1;
@@ -13,6 +17,9 @@ public class RoboticsDriver
     private float _unsafeAngleDegR0 = 0; 
     private float _unsafeAngleDegR1 = 0; 
     private float _unsafeAngleDegR2 = 0;
+    private float _unsafeVerticality;
+    
+    private Vector3 _transitionalCoordinate;
     
     private float _offsetJoystickTargetL0;
     private float _offsetJoystickTargetL1;
@@ -24,9 +31,9 @@ public class RoboticsDriver
     private float _safeJoystickTargetL0;
     private float _safeJoystickTargetL1;
     private float _safeJoystickTargetL2;
-    private float _safeAngleDegR0 = 0; 
-    private float _safeAngleDegR1 = 0; 
-    private float _safeAngleDegR2 = 0; 
+    private float _safeAngleDegTargetR0 = 0; 
+    private float _safeAngleDegTargetR1 = 0; 
+    private float _safeAngleDegTargetR2 = 0;
 
     public void ProvideTargets(InterpretedLightData interpretedData)
     {
@@ -36,27 +43,63 @@ public class RoboticsDriver
             // we don't immediately slam the robotic arm because the data has changed too much.
             return;
         }
-        
-        // TODO: Handle what happens when the target is way, way off the system.
-        // For instance we may have to consider using a PID controller in order to relativize the position,
-        // and only consider (0, 0, 0) as being a preferred position.
 
-        _unsafeJoystickTargetL0 = RemapAndClamp(interpretedData.position.Y * _virtualScaleChange, 0f, 1f, -1f, 1f);
-        _unsafeJoystickTargetL1 = RemapAndClamp(-interpretedData.position.Z * _virtualScaleChange, -0.5f, 0.5f, -1f, 1f);
-        _unsafeJoystickTargetL2 = RemapAndClamp(interpretedData.position.X * _virtualScaleChange, -0.5f, 0.5f, -1f, 1f);
-        _safeJoystickTargetL0 = Clamp(_unsafeJoystickTargetL0 + _offsetJoystickTargetL0, -1f, 1f);
-        _safeJoystickTargetL1 = Clamp(_unsafeJoystickTargetL1 + _offsetJoystickTargetL1, -1f, 1f);
-        _safeJoystickTargetL2 = Clamp(_unsafeJoystickTargetL2 + _offsetJoystickTargetL2, -1f, 1f);
-
-        if (interpretedData.hasNormal)
+        // ## Acquire Inputs
         {
-            // Normals have no twist, so we cannot set this.
-            // _unsafeAngleDegL0 = 0;
-            _unsafeAngleDegR1 = NormalToDegrees(-interpretedData.normal.X);
-            _unsafeAngleDegR2 = NormalToDegrees(-interpretedData.normal.Z);
-            _safeAngleDegR0 = Clamp(_unsafeAngleDegR0 + _offsetAngleDegR0, -360f, 360f);
-            _safeAngleDegR1 = Clamp(_unsafeAngleDegR1 + _offsetAngleDegR1, -65f, 65f);
-            _safeAngleDegR2 = Clamp(_unsafeAngleDegR2 + _offsetAngleDegR2, -65f, 65f);
+            // TODO: Handle what happens when the target is way, way off the system.
+            // For instance we may have to consider using a PID controller in order to relativize the position,
+            // and only consider (0, 0, 0) as being a preferred position.
+
+            // Confine the input light position to a centered box and make it match the robotics coordinate system.
+            _unsafeJoystickTargetL0 = RemapAndClamp(interpretedData.position.Y / _configVirtualScaleChange, 0f, 1f, -1f, 1f);
+            _unsafeJoystickTargetL1 = RemapAndClamp(-interpretedData.position.Z / _configVirtualScaleChange, -0.5f, 0.5f, -1f, 1f);
+            _unsafeJoystickTargetL2 = RemapAndClamp(interpretedData.position.X / _configVirtualScaleChange, -0.5f, 0.5f, -1f, 1f);
+            _unsafeVerticality = (_unsafeJoystickTargetL0 + 1) / 2f;
+
+            if (interpretedData.hasNormal)
+            {
+                // Perform a normal to degree conversion. This limits the range from -90 to +90.
+                _unsafeAngleDegR0 = 0; // Normals have no twist, so we cannot set this.
+                _unsafeAngleDegR1 = NormalToDegrees(-interpretedData.normal.X);
+                _unsafeAngleDegR2 = NormalToDegrees(-interpretedData.normal.Z);
+            }
+        }
+        
+        // ## From there on, we use the robotic arm coordinate space, where X is up (!!!)
+
+        if (_configSafetyUsePolarMode)
+        {
+            // When the Safety Polar mode is enabled, we clamp the Y and Z axis to be within a disc.
+            // If the length on (Y, Z) is greater than the allowed radius, we clamp it to that radius. 
+            
+            var allowedRadius = RemapAndClamp(_unsafeVerticality, 0f, 1f, _configPolarModeBottommostRadius, _configPolarModeUppermostRadius);
+            var radial = new Vector3(0, _unsafeJoystickTargetL1, _unsafeJoystickTargetL2);
+            if (radial.Length() > allowedRadius) radial = Vector3.Normalize(radial) * allowedRadius;
+            
+            _transitionalCoordinate = new Vector3(_unsafeJoystickTargetL0, radial.Y, radial.Z);
+        }
+        else
+        {
+            // Otherwise, we use unclamped coordinates. This can be dangerous.
+            
+            _transitionalCoordinate = new Vector3(_unsafeJoystickTargetL0, _unsafeJoystickTargetL1, _unsafeJoystickTargetL2);
+        }
+
+        // ## Calculate Outputs
+        {
+            // Apply offsets to the physical device. Note that doing this will reduce the motion range of the device
+            // because the input was already clamped.
+            // Using offsets instead of reducing the motion space has the advantage that the motion in virtual space
+            // is still consistent in scale in comparison to the other axis.
+            _safeJoystickTargetL0 = Clamp(_transitionalCoordinate.X + _offsetJoystickTargetL0, -1f, 1f);
+            _safeJoystickTargetL1 = Clamp(_transitionalCoordinate.Y + _offsetJoystickTargetL1, -1f, 1f);
+            _safeJoystickTargetL2 = Clamp(_transitionalCoordinate.Z + _offsetJoystickTargetL2, -1f, 1f);
+
+            // Apply offsets to the physical device and clamp it. Since the input was not clamped,
+            // this will not reduce the motion range of the device.
+            _safeAngleDegTargetR0 = Clamp(_unsafeAngleDegR0 + _offsetAngleDegR0, -360f, 360f);
+            _safeAngleDegTargetR1 = Clamp(_unsafeAngleDegR1 + _offsetAngleDegR1, -65f, 65f);
+            _safeAngleDegTargetR2 = Clamp(_unsafeAngleDegR2 + _offsetAngleDegR2, -65f, 65f);
         }
     }
 
@@ -76,9 +119,9 @@ public class RoboticsDriver
             JoystickTargetL0 = _safeJoystickTargetL0,
             JoystickTargetL1 = _safeJoystickTargetL1,
             JoystickTargetL2 = _safeJoystickTargetL2,
-            AngleDegR0 = _safeAngleDegR0,
-            AngleDegR1 = _safeAngleDegR1,
-            AngleDegR2 = _safeAngleDegR2,
+            AngleDegR0 = _safeAngleDegTargetR0,
+            AngleDegR1 = _safeAngleDegTargetR1,
+            AngleDegR2 = _safeAngleDegTargetR2,
         };
     }
 
